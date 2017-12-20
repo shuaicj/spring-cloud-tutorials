9fe3be547f00cbff64771f276480e5ba1a4fbae7
shuaicj <shuaicj@gmail.com>
2017-12-20 23:10:45 +0800

    tt11-eureka-standalone remove unused variable

--- tt11-eureka-standalone/tt11-eureka-standalone-client/src/main/java/shuaicj/tutorial/cloud/tt11/client/HelloController.java
+++ tt11-eureka-standalone/tt11-eureka-standalone-client/src/main/java/shuaicj/tutorial/cloud/tt11/client/HelloController.java
@@ -1,15 +1,14 @@
 package shuaicj.tutorial.cloud.tt11.client;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Rest controller.
@@ -20,9 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-
-    @Value("${spring.application.name}")
-    private String name;
 
     @Autowired
     private DiscoveryClient discoveryClient;
