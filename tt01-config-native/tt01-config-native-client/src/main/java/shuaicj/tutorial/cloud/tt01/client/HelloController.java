cb143f25d566290c283c0f9e0e9e0b0dd69e5598
shuaicj <shuaicj@gmail.com>
2018-10-17 15:18:25 +0800

    inject list and map via spring-cloud-config

--- tt01-config-native/tt01-config-native-client/src/main/java/shuaicj/tutorial/cloud/tt01/client/HelloController.java
+++ tt01-config-native/tt01-config-native-client/src/main/java/shuaicj/tutorial/cloud/tt01/client/HelloController.java
@@ -1,5 +1,8 @@
 package shuaicj.tutorial.cloud.tt01.client;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +21,24 @@ public class HelloController {
     @Value("${hello.message}")
     private String message;
 
+    @Value("${hello.chars}")
+    private List<String> chars;
+
+    @Value("#{${hello.dict}}")
+    private Map<String, Integer> dict;
+
     @GetMapping("/hello")
     public String hello() {
         return "port: " + port + ", message: " + message + "\n";
+    }
+
+    @GetMapping("/chars")
+    public String chars() {
+        return chars + "\n";
+    }
+
+    @GetMapping("/dict")
+    public String dict() {
+        return dict + "\n";
     }
 }
